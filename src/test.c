6a5d881cf274e96c73b004fc5716a2a381c488ba
Alf Sebastian Houge <mr.coolsebastian@hotmail.com>
2020-03-14 18:11:47 +0100

    Set up GLFW and glad with cmake (Took a lot of time, okay...)

---
+++ src/test.c
@@ -0,0 +1,12 @@
+#include <glad/glad.h>
+// Glad needs to be before GLFW
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+int main(void)
+{
+    glfwInit();
+
+    printf("Test\n");
+    return 0;
+}
