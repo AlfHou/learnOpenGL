8c225cf7b3d29497b97af8b725052e1cd71950a1
Alf Sebastian Houge <mr.coolsebastian@hotmail.com>
2020-03-15 12:44:53 +0100

    Draw triangle

--- src/test.c
+++ src/test.c
@@ -7,6 +7,22 @@
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
 
+// Vertex shader in GLSL
+const char* vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+
+// Fragment shader in GLSL
+const char* fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "}\0";
+
 // Callback from GLFW that the window was resized
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -42,14 +58,97 @@ int main(void)
         return -1;
     }
 
-    // TODO: Try to have smaller size
     glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // Set up vertices for triangle
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0,
+        0.0f, 0.5f, 0.0f
+    };
+
+    // Create Vertex Array Object
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
+    glBindVertexArray(VAO);
+
+    // Get buffer id
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+
+    // Bind buffer to a buffer type
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // Copy vertices to GPU memory
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Get vertex shader ID
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    // Add our shader source code to the vertex shader and compile
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Check compilation errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        printf("Failed to compile vertex shader: %s\n", infoLog);
+    }
+
+    // Get fragment shader id
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Add fragment shader source code and compile
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        printf("Failed to compile fragment shader: %s\n", infoLog);
+    }
+
+    // Link shader program
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Check link status
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        printf("Failed to link shader program: %s\n", infoLog);
+    }
+
+    // Delete shaders (they are now linked into the shader program)
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Specify how opengl should interpret our verticies
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
     // Render loop:
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
